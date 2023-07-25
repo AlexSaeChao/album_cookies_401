b58fa2bf36fd6a414618396916d02df267df691c
AlexSaeChao <chaoalex93@gmail.com>
2023-07-24 18:05:21 -0700

    initial commit

---
+++ src/main/java/com/java401/AlbumCookies401/AlbumCookies401Application.java
@@ -0,0 +1,13 @@
+package com.java401.AlbumCookies401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumCookies401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumCookies401Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/java401/AlbumCookies401/AlbumCookies401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java401.AlbumCookies401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumCookies401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
